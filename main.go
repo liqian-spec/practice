741dbb23221329b871b742b2541b697e6481683e
liqianqian <lqq@xiaoliuyisheng.com>
2023-04-26 11:52:08 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello 世界!")
+	r := gin.New()
+	// 处理404请求
+	r.NoRoute(func(c *gin.Context) {
+		//获取标头信息的Accept信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			//如果是html的话
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			// 默认返回JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认URL和请求方法是否正确。",
+			})
+		}
+	})
+
+	//运行服务
+	r.Run(":8000")
 }
