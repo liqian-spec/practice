e03792354f6f3159b2f5588c7cc9886e25061ab9
liqianqian <lqq@xiaoliuyisheng.com>
2023-04-27 11:39:54 +0800

    用户模型

---
+++ models/user/user_model.go
@@ -0,0 +1,15 @@
+package user
+
+import "github.com/practice/models"
+
+// User 用户模型
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
