3f35b5ef771d60eeef287c81d1dfcc1770317c23
liqianqian <lqq@xiaoliuyisheng.com>
2023-04-28 14:18:42 +0800

    数据库请求日志

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -6,10 +6,10 @@ import (
 	"github.com/practice/models/user"
 	"github.com/practice/pkg/config"
 	"github.com/practice/pkg/database"
+	"github.com/practice/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"time"
 )
 
@@ -41,7 +41,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
