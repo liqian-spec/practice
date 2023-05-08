85945f8f3b7ec70db0d4d58740cdb2d0862e72f2
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-08 11:18:53 +0800

    Email + 邮件验证码注册

--- routes/api.go
+++ routes/api.go
@@ -28,6 +28,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 判断 Email 是否注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
