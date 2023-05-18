b53c7fee18a20b21d8399272fb15c55b7aae6842
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-18 15:31:49 +0800

    migrate up 命令

---
+++ app/cmd/migrate.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/practice/database/migrations"
+	"github.com/practice/pkg/migrate"
+	"github.com/spf13/cobra"
+)
+
+var CmdMigrate = &cobra.Command{
+	Use:   "migrate",
+	Short: "Run database migration",
+}
+
+var CmdMigrateUp = &cobra.Command{
+	Use:   "up",
+	Short: "Run unmigrated migrations",
+	Run:   runUp,
+}
+
+func init() {
+	CmdMigrate.AddCommand(
+		CmdMigrateUp,
+	)
+}
+
+func migrator() *migrate.Migrator {
+	migrations.Initialize()
+	return migrate.NewMigrator()
+}
+
+func runUp(cmd *cobra.Command, args []string) {
+	migrator().Up()
+}
--- pkg/migrate/migrator.go
+++ pkg/migrate/migrator.go
@@ -1,8 +1,11 @@
 package migrate
 
 import (
+	"github.com/practice/pkg/console"
 	"github.com/practice/pkg/database"
+	"github.com/practice/pkg/file"
 	"gorm.io/gorm"
+	"os"
 )
 
 type Migrator struct {
@@ -36,4 +39,72 @@ func (migrator *Migrator) createMigrationsTable() {
 	if !migrator.Migrator.HasTable(&migration) {
 		migrator.Migrator.CreateTable(&migration)
 	}
+}
+
+func (migrator *Migrator) Up() {
+
+	migrateFiles := migrator.readAllMigrationFiles()
+
+	batch := migrator.getBatch()
+
+	migrations := []Migration{}
+	migrator.DB.Find(&migrations)
+
+	runed := false
+
+	for _, mfile := range migrateFiles {
+
+		if mfile.isNotMigrated(migrations) {
+			migrator.runUpMigration(mfile, batch)
+			runed = true
+		}
+	}
+
+	if !runed {
+		console.Success("database is up to date.")
+	}
+}
+
+func (migrator *Migrator) getBatch() int {
+
+	batch := 1
+
+	lastMigration := Migration{}
+	migrator.DB.Order("id DESC").First(&lastMigration)
+
+	if lastMigration.ID > 0 {
+		batch = lastMigration.Batch + 1
+	}
+	return batch
+}
+
+func (migrator *Migrator) readAllMigrationFiles() []MigrationFile {
+
+	files, err := os.ReadDir(migrator.Folder)
+	console.ExitIf(err)
+
+	var migrateFiles []MigrationFile
+	for _, f := range files {
+
+		fileName := file.FileNameWithoutExtension(f.Name())
+
+		mfile := getMigrationFile(fileName)
+
+		if len(mfile.FileName) > 0 {
+			migrateFiles = append(migrateFiles, mfile)
+		}
+	}
+	return migrateFiles
+}
+
+func (migrator *Migrator) runUpMigration(mfile MigrationFile, batch int) {
+
+	if mfile.Up != nil {
+		console.Warning("migrating" + mfile.FileName)
+		mfile.Up(database.DB.Migrator(), database.SQLDB)
+		console.Success("migrated " + mfile.FileName)
+	}
+
+	err := migrator.DB.Create(&Migration{Migration: mfile.FileName, Batch: batch}).Error
+	console.ExitIf(err)
 }
