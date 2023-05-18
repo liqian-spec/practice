7523bfe287c109f45b0bf888cdd79f17fa9e74a5
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-18 19:55:40 +0800

    migrate fresh 命令

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -23,6 +23,7 @@ func init() {
 		CmdMigrateRollback,
 		CmdMigrateRefresh,
 		CmdMigrateReset,
+		CmdMigrateFresh,
 	)
 }
 
@@ -64,4 +65,14 @@ var CmdMigrateRefresh = &cobra.Command{
 
 func runRefresh(cmd *cobra.Command, args []string) {
 	migrator().Refresh()
+}
+
+var CmdMigrateFresh = &cobra.Command{
+	Use:   "fresh",
+	Short: "Drop all tables and re-run all migrations",
+	Run:   runFresh,
+}
+
+func runFresh(cmd *cobra.Command, args []string) {
+	migrator().Fresh()
 }
