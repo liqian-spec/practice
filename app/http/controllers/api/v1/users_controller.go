add53b4930086a91885699067db1160097a2abf2
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-19 14:39:29 +0800

    验证分页请求

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/practice/app/models/user"
+	"github.com/practice/app/requests"
 	"github.com/practice/pkg/auth"
 	"github.com/practice/pkg/response"
 )
@@ -17,6 +18,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 }
 
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data":  data,
