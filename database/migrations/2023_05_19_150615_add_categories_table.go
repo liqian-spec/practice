5b2dd759d331042dc467dab0a8836751a4df3578
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-19 15:18:30 +0800

    分类模型

---
+++ database/migrations/2023_05_19_150615_add_categories_table.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/practice/app/models"
+	"github.com/practice/pkg/migrate"
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2023_05_19_150615_add_categories_table", up, down)
+}
