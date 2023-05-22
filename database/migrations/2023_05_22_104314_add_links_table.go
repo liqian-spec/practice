84f324c5bb755ca8c8f71fa1a80e6b2e09c349e6
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-22 10:52:24 +0800

    友情链接模型和迁移

---
+++ database/migrations/2023_05_22_104314_add_links_table.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/practice/app/models"
+	"github.com/practice/pkg/migrate"
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2023_05_22_104314_add_links_table", up, down)
+}
