1a414999476a12c2875eb34a61af516c8a8f9d0f
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-22 15:02:53 +0800

    修改邮箱

--- routes/api.go
+++ routes/api.go
@@ -46,6 +46,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		{
 			usersGroup.GET("", uc.Index)
 			usersGroup.PUT("", middlewares.AuthJWT(), uc.UpdateProfile)
+			usersGroup.PUT("/email", middlewares.AuthJWT(), uc.UpdateEmail)
 		}
 
 		cgc := new(controllers.CategoriesController)
