f5c038067f888898d4c227e54799c912730b478a
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-22 17:19:04 +0800

    强制 User Agent 中间价

---
+++ app/http/middlewares/force_ua.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/practice/pkg/response"
+)
+
+func ForceUA() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		if len(c.Request.Header["User-Agent"]) == 0 {
+			response.BadRequest(c, errors.New("User-Agent 标头未找到"), "请求必须附带 User-Agent 标头")
+			return
+		}
+
+		c.Next()
+	}
+}
