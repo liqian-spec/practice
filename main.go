ce4410ede11347fa0d9758be306a0b44e5f6f1de
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-24 16:51:21 +0800

    集成 gin

--- main.go
+++ main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"Hello": "World!",
 		})
 	})
 
-	if err := router.Run(":3000"); err != nil {
-		log.Fatal("Server Run Failed.: ", err)
-	}
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请先确认 url 和请求方法是否正确.",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
