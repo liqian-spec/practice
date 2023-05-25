3eb51c7e3028a1ded54ae88231bb3261aa43d7a3
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-25 15:14:29 +0800

    用户模型

---
+++ app/models/user/user_model.go
@@ -0,0 +1,14 @@
+package user
+
+import "github.com/liqian-spec/practice/app/models"
+
+type User struct {
+	models.BaseModel
+
+	Name     string `json:"name,omitempty"`
+	Email    string `json:"-"`
+	Phone    string `json:"-"`
+	Password string `json:"-"`
+
+	models.CommonTimestampsField
+}
---
+++ app/models/user/user_util.go
@@ -0,0 +1,15 @@
+package user
+
+import "github.com/liqian-spec/practice/pkg/database"
+
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
+
+func IsPhoneExist(phone string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("phone = ?", phone).Count(&count)
+	return count > 0
+}
