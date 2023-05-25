5cdc575724397b6b1e8a37e927f4dfebceaa0ebc
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-25 11:49:36 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/liqian-spec/practice/bootstrap"
+	"github.com/liqian-spec/practice/pkg/config"
+
+	btsConfig "github.com/liqian-spec/practice/config"
 )
 
+func init() {
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	router := gin.New()
 
 	bootstrap.SetupRoute(router)
 
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
