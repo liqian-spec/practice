b1b292fe92f240412f81f2a375467c70a3e72370
liqianqian <lqq@xiaoliuyisheng.com>
2023-05-31 11:49:50 +0800

    使用密码登录

--- routes/api.go
+++ routes/api.go
@@ -24,6 +24,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 
 			lgc := new(auth.LoginController)
 			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
+			authGroup.POST("/login/using-password", lgc.LoginByPassword)
 		}
 	}
 }
