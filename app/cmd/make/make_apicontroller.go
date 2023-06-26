7f2e47bfc456016c70c149bc22b94c138ea8db46
liqianqian <lqq@xiaoliuyisheng.com>
2023-06-26 17:05:42 +0800

    cmd controller fix

--- app/cmd/make/make_apicontroller.go
+++ app/cmd/make/make_apicontroller.go
@@ -4,27 +4,48 @@ import (
 	"fmt"
 	"github.com/liqian-spec/practice/pkg/console"
 	"github.com/spf13/cobra"
+	"os"
 	"strings"
 )
 
+var version string
+
 var CmdMakeAPIController = &cobra.Command{
-	Use:   "apicontroller",
-	Short: "Create api controller, example: make apicontroller v1/user",
-	Run:   runMakeAPIController,
-	Args:  cobra.ExactArgs(1),
+	Use:     "apicontroller",
+	Short:   "Create api controller",
+	Run:     runMakeAPIController,
+	Args:    cobra.ExactArgs(1),
+	Example: "make apicontroller user --v=2",
 }
 
-func runMakeAPIController(cmd *cobra.Command, args []string) {
+func init() {
+	CmdMakeAPIController.PersistentFlags().StringVarP(&version, "version", "v", "", "version")
+}
 
-	array := strings.Split(args[0], "/")
-	if len(array) != 2 {
-		console.Exit("api controller name format: v1/user")
+func runMakeAPIController(command *cobra.Command, args []string) {
+
+	dirs := strings.Split(args[0], "/")
+	if len(dirs) > 3 {
+		console.Exit("The directory supports up to three levels only, And the last name is the file name")
 	}
 
-	apiVersion, name := array[0], array[1]
-	model := makeModelFromString(name)
+	version = fmt.Sprintf("%s", version)
 
-	filePath := fmt.Sprintf("app/http/controllers/api/%s/%s_controller.go", apiVersion, model.TableName)
+	dirPath := fmt.Sprintf("app/http/controllers/api/%s", version)
+	if len(dirs) > 1 {
+		for i := 0; i < len(dirs)-1; i++ {
+			dirPath += dirs[i] + "/"
+		}
+	}
 
-	createFileFromStub(filePath, "apicontroller", model)
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	if err != nil {
+		console.ExitIf(err)
+	}
+
+	model := makeModelFromString(dirs[len(dirs)-1])
+
+	fileName := fmt.Sprintf("%s_contoller.go", model.PackageName)
+
+	createFileFromStub(dirPath+fileName, "apicontroller", model, map[string]string{"{{version}}": version})
 }
