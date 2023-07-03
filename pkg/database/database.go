bb385cd0dcf7fdd0384280c0f0f4e6b0331a1dc5
liqianqian <lqq@xiaoliuyisheng.com>
2023-07-03 14:47:20 +0800

    migrate fresh 命令

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+	"github.com/liqian-spec/practice/pkg/config"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 )
@@ -24,4 +26,62 @@ func Connect(dbConfig gorm.Dialector, _logger gormlogger.Interface) {
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func CurrentDatabase() (dbname string) {
+	dbname = DB.Migrator().CurrentDatabase()
+	return
+}
+
+func DeleteAllTables() error {
+	var err error
+	switch config.Get("database.connection") {
+	case "mysql":
+		err = deleteMySQLTables()
+	case "sqlite":
+		err = deleteAllSqliteTables()
+	default:
+		panic(errors.New("database connection not supported"))
+	}
+
+	return err
+}
+
+func deleteAllSqliteTables() error {
+	tables := []string{}
+
+	err := DB.Select(&tables, "SELECT name FROM sqlite_master WHERE type='table'").Error
+	if err != nil {
+		return err
+	}
+
+	for _, table := range tables {
+		err := DB.Migrator().DropTable(table)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteMySQLTables() error {
+	dbname := CurrentDatabase()
+	tables := []string{}
+
+	err := DB.Table("information_schema.tables").Where("table_schema = ?", dbname).Pluck("table_name", &tables).Error
+	if err != nil {
+		return err
+	}
+
+	DB.Exec("SET foreign_key_checks = 0;")
+
+	for _, table := range tables {
+		err := DB.Migrator().DropTable(table)
+		if err != nil {
+			return err
+		}
+	}
+
+	DB.Exec("SET foreign_key_checks = 1;")
+	return nil
 }
