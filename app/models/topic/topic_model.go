1cca061f64d249e59f990d67f41b9887822a5708
liqianqian <lqq@xiaoliuyisheng.com>
2023-07-05 17:20:45 +0800

    话题模型和迁移

---
+++ app/models/topic/topic_model.go
@@ -0,0 +1,37 @@
+package topic
+
+import (
+	"github.com/liqian-spec/practice/app/models"
+	"github.com/liqian-spec/practice/app/models/category"
+	"github.com/liqian-spec/practice/app/models/user"
+	"github.com/liqian-spec/practice/pkg/database"
+)
+
+type Topic struct {
+	models.BaseModel
+
+	Title      string `json:"title,omitempty"`
+	Body       string `json:"body,omitempty"`
+	UserID     string `json:"user_id,omitempty"`
+	CategoryID string `json:"category_id,omitempty"`
+
+	User user.User `json:"user"`
+
+	Category category.Category `json:"category"`
+
+	models.CommonTimestampsField
+}
+
+func (topic *Topic) Create() {
+	database.DB.Create(&topic)
+}
+
+func (topic *Topic) Save() (rowsAffected int64) {
+	result := database.DB.Save(&topic)
+	return result.RowsAffected
+}
+
+func (topic *Topic) Delete() (rowsAffected int64) {
+	result := database.DB.Delete(&topic)
+	return result.RowsAffected
+}
---
+++ database/migrations/2023_07_05_171348_add_topics_table.go
@@ -0,0 +1,43 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/liqian-spec/practice/app/models"
+	"github.com/liqian-spec/practice/pkg/migrate"
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+	}
+
+	type Category struct {
+		models.BaseModel
+	}
+
+	type Topic struct {
+		models.BaseModel
+
+		Title      string `gorm:"type:varchar(255);not null;index"`
+		Body       string `gorm:"type:longtext;not null"`
+		UserID     string `gorm:"type:bigint;not null;index"`
+		CategoryID string `gorm:"type:bigint;not null;index"`
+
+		User     User
+		Category Category
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Topic{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Topic{})
+	}
+
+	migrate.Add("2023_07_05_171348_add_topics_table", up, down)
+}
